7262dcda6523b36a4b74e2b7c6c9568bffadc558
mateusz <skoram18@gmial.com>
2026-04-09 23:19:44 +0200

    fixed api response error

--- SmartStore/SmartStore/React/my-react-app/src/Components/Home/ProductGrid.jsx
+++ SmartStore/SmartStore/React/my-react-app/src/Components/Home/ProductGrid.jsx
@@ -1,31 +1,67 @@
 import api from '../../api';
+import axios from 'axios';
 
-const  ProductGrid =  () => {
+import {useState, useEffect} from 'react'
+
+const  ProductGrid =   () => {
     
     const placeholders = Array(8).fill(null);
     
-    const  response =  api.get('/Home');
-    const products = response.data;
+    const [products , setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);  
+    
+    useEffect(()=>{
+        const fetchData = async () => {
+            try{
+                const response =  await axios.get('http://localhost:5144/api/Home');
+                
+               
+                setProducts(response.data.products || response.data.data);
+                
+            }
+            catch(err){
+                console.error("Błąd pobierania:",err);
+            }
+            finally{
+                setLoading(false);
+            }
+            
+        }
+
+
+        fetchData();
+
+    },[]);
+    
     
 
-    return (
-        <div className="product-grid">
-            {products.map((product) => (
-                <div key={product.id} className="product-card">
-                    <div className="product-card-img">
-                        <div className="img-placeholder">
-                            <img src={product.images} alt={product.title} />
+    if (loading) {
+        return <div className="p-8 text-center text-gray-500">Ładowanie produktów z serwera...</div>;
+    }
+   
+    else{
+       
+        return (
+
+            <div className="product-grid">
+                {products.map((product) => (
+                    <div key={product.id} className="product-card">
+                        <div className="product-card-img">
+                            <div className="img-placeholder">
+                                <img src={product.images[0]} width="100" height="100"  alt={product.title} />
+                            </div>
+                        </div>
+                        <div className="product-card-info">
+                            <span className="card-brand">{product.category}</span>
+                            <h4>{product.title}</h4>
+                            <p className="card-price">{product.price}PLN</p>
                         </div>
                     </div>
-                    <div className="product-card-info">
-                        <span className="card-brand">{product.category}</span>
-                        <h4>{product.title}</h4>
-                        <p className="card-price">{product.price}PLN</p>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
+                ))}
+            </div>
+        );
+    }
+    
 };
 
 export default ProductGrid;
