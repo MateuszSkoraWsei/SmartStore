1560ebbc17126b1f1e886544ffd89dfb9155af7e
mateusz <skoram18@gmial.com>
2026-04-22 00:09:16 +0200

    accidentaly restarted serwer

--- SmartStore/SmartStore/obj/Debug/net8.0/SmartStore.AssemblyInfo.cs
+++ SmartStore/SmartStore/obj/Debug/net8.0/SmartStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40cb9dfc4f9a172bf48d45fd245d978044086bd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95ce59db200d6449367cd3463447d255f87bf6de")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
