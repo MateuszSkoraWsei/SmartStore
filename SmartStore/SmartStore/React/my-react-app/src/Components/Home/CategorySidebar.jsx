e825815c1f331f9218f58216b38ec888a826a127
mateusz <skoram18@gmial.com>
2026-04-22 23:16:06 +0200

    fetching category date from api

--- SmartStore/SmartStore/React/my-react-app/src/Components/Home/CategorySidebar.jsx
+++ SmartStore/SmartStore/React/my-react-app/src/Components/Home/CategorySidebar.jsx
@@ -1,19 +1,47 @@
+import { useState, useEffect } from 'react'
+import  axios  from "axios";
+
+
 const CategorySidebar = () => {
     
-    const categories = ["Wszystkie", "Smartfony", "Laptopy", "Zegarki", "Audio", "Akcesoria", "Gaming", "Aparaty", "AGD", "Dom inteligentny"];
+    const [categories, setCategories] = useState(true);
+    const [loading, setLoading] = useState(true);
+    
+    useEffect( () => async () =>{
+        try{
+            const response = await axios.get("http://localhost:5144/api/Categories/getAll");
+            
+            setCategories(response.data.data);
+            
+        }
+        catch(err){
+            setLoading(true);
+            console.log(err);
+        }
+        finally{
+            setLoading(false);
+        }
+    },[])
 
-    return (
-        <aside className="category-sidebar">
-            <h3 className="sidebar-heading">Kategorie</h3>
-            <nav className="sidebar-nav">
-                {categories.map((cat, index) => (
-                    <button key={index} className="sidebar-link">
-                        {cat}
-                    </button>
-                ))}
-            </nav>
-        </aside>
-    );
+    
+    if(loading){
+        <div>Ładowanie kategorii</div>
+    }
+    else{
+        return (
+            <div className="category-sidebar">
+                <h3 className="sidebar-heading">Kategorie</h3>
+                <nav className="sidebar-nav category-flex">
+                    {categories.map((cat, index) => (
+                        <button key={index} className="sidebar-link">
+                            {cat}
+                        </button>
+                    ))}
+                </nav>
+            </div>
+        );
+    }
+    
 };
 
 export default CategorySidebar;
